af340a6d4687c45504c3031fd394b75adf3385cb
Tahir_Rafique <tahir.rafique@siparadigm.com>
2023-06-15 12:31:53 +0500

    UPDATE, adding source files.

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 # vscode-icons
-This package allow to use the vscode icons with web applications that are written in python (Django).
+Django app to provide vscode icons for web application.
 
 Please refer to [original author](https://marketplace.visualstudio.com/items?itemName=vscode-icons-team.vscode-icons) for more detail.
+
+## Installation
+
+Following command will help to install the package.
+
+```bash
+pip install vscode-icons
+```
+
+## Setup
+
+1. Add the app to **setting.py** file in **INSTALLED_APPS** section.
+
+```python
+INSTALLED_APPS = [
+    ...
+    'vscode_icons',
+]
+```
+
+## Usage
+
+```python
+from vscode_icons.vscode import VSCodeIcons
+
+vsi = VSCodeIcons()
+
+# Getting file path
+file_name = 'image.png'
+file_icon_path = vsi.findFileIcon(file_name)
+print(f'File Icon: {file_icon_path}')
+
+# Getting directory path
+dir_name = 'Documents'
+dir_icon_path = vsi.findDirectoryIcon(dir_name)
+print(f'Directory Icon: {dir_icon_path}')
+```
+
+#### Output
+
+```bash
+File Icon: vscode_icons/icons/file_type_image.svg
+Directory Icon: vscode_icons/icons/default_folder.svg
+```
+
+## Author
+
+**Tahir Rafique**
---
+++ vscode.py
@@ -0,0 +1,70 @@
+""" 
+Author:		 Muhammad Tahir Rafique
+Date:		 2023-02-27 12:26:11
+Project:	 Explorer
+Description: Provide function to use the vscode icons.
+"""
+
+import os
+import json
+
+class VSCodeIcons():
+    def __init__(self) -> None:
+        # DEFINING NAMES
+        module_name = 'vscode_icons'
+        icon_dir = 'icons'
+        info_file_name = 'vsicons-icon-theme.json'
+        
+        # GETTING STATIC ROOT
+        try:
+            from django.conf import settings
+            static_root = os.path.join(settings.STATIC_ROOT, module_name)
+        except:
+            static_root = os.path.join(os.path.dirname(__file__), 'static', module_name)
+        if not os.path.exists(static_root):
+            static_root = os.path.join(os.path.dirname(__file__), 'static', module_name)
+        
+        # GETTING ICON DIR PATH
+        self.icon_root = os.path.join(module_name, icon_dir)
+        
+        # READING INFO
+        try:
+            with open(os.path.join(static_root, info_file_name), 'rb') as f:
+                self.info = json.load(f)
+        except:
+            self.info = None
+        return None
+    
+    def findDirectoryIcon(self, name):
+        """Finding best possible icon for directory."""
+        # FINDING ICON
+        icon_name = self.info['folderNames'].get(name)
+        if icon_name is None: # Default condition
+            icon_name = '_folder'
+        
+        # FINDING ICON PATH
+        icon_path = self.info['iconDefinitions'][icon_name]
+        icon_name = os.path.basename(icon_path['iconPath'])
+        
+        # FINDING STATIC PATH
+        static_path = os.path.join(self.icon_root, icon_name)
+        return static_path
+        
+    def findFileIcon(self, name):
+        """Finding best possible icon for file."""
+        # FINDING ICON
+        icon_name = self.info['fileNames'].get(name)
+        if icon_name is None:
+            # GETTING ON THE BASE OF EXT
+            ext = os.path.splitext(name)[-1][1:]
+            icon_name = self.info['fileExtensions'].get(ext)
+            if icon_name is None:
+                icon_name = '_file' # Default Condition
+        
+        # FINDING ICON PATH
+        icon_path = self.info['iconDefinitions'][icon_name]
+        icon_name = os.path.basename(icon_path['iconPath'])
+        
+        # FINDING STATIC PATH
+        static_path = os.path.join(self.icon_root, icon_name)
+        return static_path
